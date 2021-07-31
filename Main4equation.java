dfd9782cf71abfa7c27550aa2cd19786cd82ed43
Hyunjin-Noh <88017666+Hyunjin-Noh@users.noreply.github.com>
2021-07-31 11:04:56 +0900

    Update Main4equation.java

--- Main4equation.java
+++ Main4equation.java
@@ -1,3 +1,26 @@
+/*p.112 실습문제12
+예시 입력 1 
+Input a equation >> 2 + 4
+예시 출력 1
+2.0 + 4.0 = 6.0
+예시 입력 2 
+Input a equation >> 100 * 7
+예시 출력 2
+100.0 * 7.0 = 700.0
+예시 입력 3 
+Input a equation >> 5 / 2
+예시 출력 3
+5.0 / 2.0 = 2.5
+예시 입력 4 
+Input a equation >> 14 / 0
+예시 출력 4
+Can not divide by 0
+예시 입력 5 
+Input a equation >> 7.5 - 9.9
+예시 출력 5
+7.5 - 9.9 = -2.3999996
+*/
+
 import java.util.Scanner;
 
 public class Main4equation
